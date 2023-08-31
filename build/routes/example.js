9f4126df14727fd0af2235d2b0bea52861c34700
Fredo-Ronan <fredocode06@gmail.com>
2023-08-31 12:53:46 +0700

    compiled to build for production deploy

--- build/routes/example.js
+++ build/routes/example.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 router.get('/example', (req, res) => {
+    const query = req.query["query"];
+    console.log(query);
     res.json({ message: 'Example bro!' });
 });
 exports.default = router;
