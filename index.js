0f63f0bc695f94d8d8cb154d5125291e0fb3af27
Fredo-Ronan <fredocode06@gmail.com>
2023-08-31 17:11:22 +0700

    add documentation landing page ver-1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require('path'));
 const example_1 = __importDefault(require("./routes/example"));
 const bmkg_cuaca_api_1 = __importDefault(require("./routes/bmkg-cuaca-api"));
 const list_wilayah_1 = __importDefault(require("./routes/list-wilayah"));
@@ -16,7 +17,7 @@ app.use('/api', example_1.default);
 app.use('/api', bmkg_cuaca_api_1.default);
 app.use('/api', list_wilayah_1.default);
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/landing-page.html');
 
 });
 app.listen(port, () => {
